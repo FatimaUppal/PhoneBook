15789ec2bf4c848a5820768f6f75391cf1289624
FatimaUppal <125939989+FatimaUppal@users.noreply.github.com>
2023-03-17 12:23:52 -0400

    Done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,10 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import org.codehaus.plexus.interpolation.SimpleRecursionInterceptor;
+
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -13,42 +12,57 @@ import java.util.Map;
  */
 public class PhoneBook {
 
-    private final Map<String, List<String>> phonebook;
+    private Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook=map; // setting phonebook equal to map
     }
 
     public PhoneBook() {
-        this(null);
+       this.phonebook=new LinkedHashMap<>();// putting phonebook into linked hashmap so that the order wont mess up
     }
 
     public void add(String name, String phoneNumber) {
+        List<String> newNum= new ArrayList<>();
+        newNum.add(phoneNumber);
+        this.phonebook.putIfAbsent(name,newNum);
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        List<String> newNum= new ArrayList<>();
+        newNum.addAll(List.of(phoneNumbers));
+        this.phonebook.putIfAbsent(name,newNum);
     }
 
     public void remove(String name) {
+        this.phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return this.phonebook.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return this.phonebook.get(name);
     }
 
-    public String reverseLookup(String phoneNumber)  {
-        return null;
+    public String reverseLookup(String phoneNumber) {
+        for (String number : phonebook.keySet()) {
+            if (phonebook.get(number).contains(phoneNumber)) {
+                return number;
+            }
+        }return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> list= new ArrayList<String>();             //new array list for map phonebook
+        this.phonebook.keySet().forEach(key -> list.add(key));  //uses keySet method to get set of all keys in tha map and iterates
+        return list;                                            //over all of them with forEach method and key -> list.add(key) tak
+                                                                // each key in set and adds to the list
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.phonebook;
     }
 }
--- src/test/java/com/zipcodewilmington/phonebook/ReverseLookupTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/ReverseLookupTest.java
@@ -10,8 +10,8 @@ public class ReverseLookupTest {
         PhoneBook phoneBook = new PhoneBook();
         String expectedName = "John";
         String phoneNumber = "302-555-4545";
-        phoneBook.add(expectedName, phoneNumber);
-        Assert.assertTrue(phoneBook.hasEntry(expectedName, phoneNumber));
+        phoneBook.add(expectedName,phoneNumber);
+        Assert.assertTrue(phoneBook.hasEntry( expectedName));
 
         // when
         String actualName = phoneBook.reverseLookup(phoneNumber);
@@ -27,7 +27,7 @@ public class ReverseLookupTest {
         String expectedName = "Joe";
         String phoneNumber = "302-554-4545";
         phoneBook.add(expectedName, phoneNumber);
-        Assert.assertTrue(phoneBook.hasEntry(expectedName, phoneNumber));
+        Assert.assertTrue(phoneBook.hasEntry(expectedName));
 
         // when
         String actualName = phoneBook.reverseLookup(phoneNumber);
@@ -44,7 +44,7 @@ public class ReverseLookupTest {
         String expectedName = "Smith";
         String phoneNumber = "302-554-4535";
         phoneBook.add(expectedName, phoneNumber);
-        Assert.assertTrue(phoneBook.hasEntry(expectedName, phoneNumber));
+        Assert.assertTrue(phoneBook.hasEntry(expectedName));
 
         // when
         String actualName = phoneBook.reverseLookup(phoneNumber);
